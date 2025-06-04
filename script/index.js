d638f8de0df7b6bac179e90eae7ffb9df554eda7
jakiadp <jakiadp@gmail.com>
2025-06-05 02:59:32 +0600

    add to new work

--- script/index.js
+++ script/index.js
@@ -32,8 +32,225 @@ document.getElementById("Completed_btn_1").addEventListener("click", function(x)
     x.target.style.backgroundColor = "red";
     x.target.disabled = true;
 
+
+    const task = document.getElementById("Task_Assigned").innerText;
+    const convertNumber = parseInt(task);
+
+    const biyog = convertNumber - 1;
+    document.getElementById("Task_Assigned").innerText =biyog;
+     const navNumber = document.getElementById("navbar_number").innerText;
+
+     const navbarNumber = document.getElementById("navbar_number").innerText;
+    const convertednumber = parseInt(navbarNumber);
+    const sum = convertednumber + 1;
+    document.getElementById("navbar_number").innerText = sum;
+
+
+    if(biyog === 0){
+        alert("All board undated successully")
+    }
+
+    const now = new Date();
+    const hours = now.getHours();
+    const minites = now.getMinutes();
+    const secend = now.getSeconds
+    const timeset = `${hours}: ${minites}: ${secend}`;
+    const maincontainar = document.getElementById("Activity_log1");
+    const fixMobile = document.getElementById("fix_mobile").innerText;
+    const p = document.createElement("p");
+    p.innerHTML= `you have complated the task <h1>${fixMobile}</h1>${time} `;
+    maincontainar.appendChild(p)
+
 })
 
-document.getElementById("Completed_btn_2").addEventListener("click",function(x){
-    alert("hey my you koi");
+// button number -- 2
+document.getElementById("Completed_btn_2").addEventListener("click", function(x){
+    alert("board undated successully");
+    x.target.style.backgroundColor = "red";
+x.target.disabled = true;
+
+
+    const task = document.getElementById("Task_Assigned").innerText;
+     const convertNumber = parseInt(task);
+
+     const biyog = convertNumber - 1;
+     document.getElementById("Task_Assigned").innerText = biyog;
+
+     const navbarNumber = document.getElementById("navbar_number").innerText;
+     const convertednumber = parseInt(navbarNumber);
+     const sum = convertednumber + 1;
+     document.getElementById("navbar_number").innerText = sum;
+
+     if(biyog == 0){
+        alert("All board undated successully ")
+     }
+     const now = new Date();
+     const hours = now.getHours();
+     const minutes = now.getMinutes();
+     const secend = now.getSeconds();
+     const timeSet =` ${hours}: ${minutes}: ${secend}`;
+     const maincontainar = document.getElementById("Activity_log2");
+     const fixMobile = document.getElementById("fix_mobile5").innerText;
+     const p = document.createElement("p");
+     p.innerHTML = `you have complated the task <h1>${fixMobile}</h1>${timeSet}`;
+     maincontainar.appendChild(p)
+
+})
+
+
+// button-3
+
+
+document.getElementById("Completed_btn_3").addEventListener("click", function(x){
+    alert("Board undated successully");
+    x.target.style.backgroundColor ="red";
+    x.target.disabled = true;
+
+    const task =document.getElementById("Task_Assigned").innerText;
+    const convertNumber = parseInt(task);
+    const biyog = convertNumber - 1;
+    document.getElementById("Task_Assigned").innerText = biyog;
+
+    const navbarNumber = document.getElementById("navbar_number").innerText;
+    const convertednumber = parseInt(navbarNumber);
+    const sum = convertednumber + 1;
+    document.getElementById("navbar_number").innerText = sum;
+
+       if(biyog == 0){
+        alert("All board undated successully ")}
+
+
+   const now = new Date();
+     const hours = now.getHours();
+     const minutes = now.getMinutes();
+     const secend = now.getSeconds();
+     const timeSet =` ${hours}: ${minutes}: ${secend}`;
+     const maincontainar = document.getElementById("Activity_log2");
+     const fixMobile = document.getElementById("fix_mobile6").innerText;
+     const p = document.createElement("p");
+     p.innerHTML = `you have complated the task <h1>${fixMobile}</h1>${timeSet}`;
+     maincontainar.appendChild(p)
+
+
+})
+
+
+// button-4
+
+
+document.getElementById("Completed_btn_4").addEventListener("click", function(x){
+    alert("Board undated successully");
+    x.target.style.backgroundColor ="red";
+    x.target.disabled = true;
+
+    const task =document.getElementById("Task_Assigned").innerText;
+    const convertNumber = parseInt(task);
+    const biyog = convertNumber - 1;
+    document.getElementById("Task_Assigned").innerText = biyog;
+
+    const navbarNumber = document.getElementById("navbar_number").innerText;
+    const convertednumber = parseInt(navbarNumber);
+    const sum = convertednumber + 1;
+    document.getElementById("navbar_number").innerText = sum;
+
+       if(biyog == 0){
+        alert("All board undated successully ")}
+
+
+   const now = new Date();
+     const hours = now.getHours();
+     const minutes = now.getMinutes();
+     const secend = now.getSeconds();
+     const timeSet =` ${hours}: ${minutes}: ${secend}`;
+     const maincontainar = document.getElementById("Activity_log2");
+     const fixMobile = document.getElementById("fix_mobile7").innerText;
+     const p = document.createElement("p");
+     p.innerHTML = `you have complated the task <h1>${fixMobile}</h1>${timeSet}`;
+     maincontainar.appendChild(p)
+
+
+})
+
+
+// button-5
+
+
+document.getElementById("Completed_btn_5").addEventListener("click", function(x){
+    alert("Board undated successully");
+    x.target.style.backgroundColor ="red";
+    x.target.disabled = true;
+
+    const task =document.getElementById("Task_Assigned").innerText;
+    const convertNumber = parseInt(task);
+    const biyog = convertNumber - 1;
+    document.getElementById("Task_Assigned").innerText = biyog;
+
+    const navbarNumber = document.getElementById("navbar_number").innerText;
+    const convertednumber = parseInt(navbarNumber);
+    const sum = convertednumber + 1;
+    document.getElementById("navbar_number").innerText = sum;
+
+       if(biyog == 0){
+        alert("All board undated successully ")}
+
+
+   const now = new Date();
+     const hours = now.getHours();
+     const minutes = now.getMinutes();
+     const secend = now.getSeconds();
+     const timeSet =` ${hours}: ${minutes}: ${secend}`;
+     const maincontainar = document.getElementById("Activity_log2");
+     const fixMobile = document.getElementById("fix_mobile8").innerText;
+     const p = document.createElement("p");
+     p.innerHTML = `you have complated the task <h1>${fixMobile}</h1>${timeSet}`;
+     maincontainar.appendChild(p)
+
+
+})
+
+// button-6
+
+
+document.getElementById("Completed_btn_6").addEventListener("click", function(x){
+    alert("Board undated successully");
+    x.target.style.backgroundColor ="red";
+    x.target.disabled = true;
+
+    const task =document.getElementById("Task_Assigned").innerText;
+    const convertNumber = parseInt(task);
+    const biyog = convertNumber - 1;
+    document.getElementById("Task_Assigned").innerText = biyog;
+
+    const navbarNumber = document.getElementById("navbar_number").innerText;
+    const convertednumber = parseInt(navbarNumber);
+    const sum = convertednumber + 1;
+    document.getElementById("navbar_number").innerText = sum;
+
+       if(biyog == 0){
+        alert("All board undated successully ")}
+
+
+   const now = new Date();
+     const hours = now.getHours();
+     const minutes = now.getMinutes();
+     const secend = now.getSeconds();
+     const timeSet =` ${hours}: ${minutes}: ${secend}`;
+     const maincontainar = document.getElementById("Activity_log2");
+     const fixMobile = document.getElementById("fix_mobile9").innerText;
+     const p = document.createElement("p");
+     p.innerHTML = `you have complated the task <h1>${fixMobile}</h1>${timeSet}`;
+     maincontainar.appendChild(p)
+
+
+})
+
+// clear history
+
+document.getElementById("Clear_History").addEventListener("click", function(){
+     document.getElementById('Activity_log1').innerText = "";
+ document.getElementById('Activity_log2').innerText = "";
+ document.getElementById('Activity_log3').innerText = "";
+ document.getElementById('Activity_log4').innerText = "";
+ document.getElementById('Activity_log5').innerText = "";
+ document.getElementById('Activity_log6').innerText = "";
 })
